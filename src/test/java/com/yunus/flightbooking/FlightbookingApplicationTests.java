baaa55e2f379a8e3dc9a2730eb960d44c7ed4383
Yunus Sezgin <yunussezgin8@hotmail.com>
2020-11-06 22:22:04 +0300

    project created

---
+++ src/test/java/com/yunus/flightbooking/FlightbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.flightbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
